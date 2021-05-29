46c854cfb1439edd8b90410ae1727a63f8f8ff69
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-05-29 12:04:01 +0200

    useEffect eplanation

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,7 +3,17 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setdebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setdebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const wiki = async () => {
@@ -13,21 +23,33 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
-        },
+          srsearch:debouncedTerm,
+          },
+          
       });
       setResults(data.query.search);
-    };
-    wiki();
-  }, [term]);
+      };
+      if (debouncedTerm) {
+          return wiki();
+      }
+      
+  }, [debouncedTerm]);
+
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
-                <div className="header">{result.title}</div>
-                <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
-          
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
