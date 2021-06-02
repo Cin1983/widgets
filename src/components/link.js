c6627de64cbd5e4b8830a64ea7c0e60d2278d10a
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-06-02 15:50:06 +0200

    Navigation without router update

--- src/components/link.js
+++ src/components/link.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
     const onClick = (e) => {
+        if (e.metaKey || e.ctrlKey) {
+            return;
+}
+
         e.preventDefault();
-        window.history.pushState({}, '',href)
-    }
+        window.history.pushState({}, '', href)
+        
+        const navEvent = new PopStateEvent('popstate')
+        window.dispatchEvent(navEvent);
+        // The url has changed.
+    };
   return <a onClick={onClick} className={className} href={href} children={children}></a>;
 };
 
