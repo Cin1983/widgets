5f11a046f6e56594a29ce02cd9305890bb3a5b91
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-06-02 15:02:27 +0200

    Navigation without browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 import GoogleTranslate from "./apis/GoogleTranslate";
-// import Accordion from "./Accordion";
-// import Search from "./search/Search";
+import Accordion from "./Accordion";
+import Search from "./search/Search";
 import Dropdown from "./dropdown/Dropdown";
+import Route from "./Route";
+import Header from "./Header";
 
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework",
-//   },
-//   {
-//     title: "Why use React",
-//     content: "React is a favorite Js library among engineers",
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "You use React by creating components",
-//   },
-// ];
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React",
+    content: "React is a favorite Js library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 const options = [
   {
@@ -39,7 +42,23 @@ const App = () => {
 
   return (
     <div>
-      <GoogleTranslate />
+       <Header/>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/translate">
+        <GoogleTranslate />
+      </Route>
+      <Route path="/dropdown" >
+        <Dropdown label='Select a color'
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
     </div>
   );
 };
---
+++ src/components/link.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Link = ({ className, href, children }) => {
+    const onClick = (e) => {
+        e.preventDefault();
+        window.history.pushState({}, '',href)
+    }
+  return <a onClick={onClick} className={className} href={href} children={children}></a>;
+};
+
+export default Link;
